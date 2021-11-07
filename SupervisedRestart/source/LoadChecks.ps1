77cb8f1a70d93a3ad9ef11ec8515a619b34d941e
MartinGutz <MartinGutz@gmail.com>
2021-11-07 09:10:59 -0800

    Update LoadChecks with Parameters
    Update the parameters for the LoadPreChecksFromXML function

--- SupervisedRestart/source/LoadChecks.ps1
+++ SupervisedRestart/source/LoadChecks.ps1
@@ -1,6 +1,9 @@
-function LoadPreChecksFromXML($xmlFile)
+function LoadPreChecksFromXML
 {
-    Write-Host "Loading the config information from $xmlFile"
+    Param(
+    [Parameter(Mandatory)]
+    [string] $xmlFile)
+    Write-Verbose "Loading the config information from $xmlFile"
     [xml]$prechecks = Get-Content -Path $xmlFile
     return $prechecks.Prechecks
 }
